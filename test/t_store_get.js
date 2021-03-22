8e19ee44348871376409968a05a4469040cf90a7
David Ward <13969277+dsw6@users.noreply.github.com>
2021-03-22 15:35:38 -0600

    fix: Get item not setting error property correctly (use "error" not "err" for name)

--- test/t_store_get.js
+++ test/t_store_get.js
@@ -26,25 +26,35 @@ describe("Store Get", function ()
       //----------------------------------------------------------------------------
    it(`store.get should return item from store`, function () 
    {
-      var store = kvs({name: "myStore", maxSize: 100, itemTTL: 100});
+      var store = kvs({name: "myStore", maxSize: 100, itemTTL: 5000});
 
       store.insert("key1", "value1");
 
       var vInfo = store.get("key1");
-      expect(vInfo.error, "prop:error - invalid").to.not.exist;
+      expect(vInfo, "prop:error - not found").to.haveOwnProperty("error");
+      expect(vInfo, "prop:value - not found").to.haveOwnProperty("value");
+      expect(vInfo, "prop:type - not found").to.haveOwnProperty("type");
+
+      expect(vInfo.error, "prop:error - invalid").to.equal(undefined);
       expect(vInfo.value, "prop:value - invalid").to.equal("value1");
+      expect(vInfo.type, "prop:type - invalid").to.equal(kvs.ITEM_TYPE.VOLATILE);
    });
 
 
       //----------------------------------------------------------------------------
-   it(`store.get should not delete non-existant items`, function () 
+   it(`store.get should not return non-existant items`, function () 
    {
-      var store = kvs({name: "myStore", maxSize: 100, itemTTL: 100});
+      var store = kvs({name: "myStore", maxSize: 100, itemTTL: 5000});
 
       var vInfo = store.get("key1");
+      expect(vInfo, "prop:error - not found").to.haveOwnProperty("error");
+      expect(vInfo, "prop:value - not found").to.haveOwnProperty("value");
+      expect(vInfo, "prop:type - not found").to.haveOwnProperty("type");
+
       expect(vInfo.error, "prop:error - invalid").to.exist;
-      expect(vInfo.value, "prop:value - invalid").to.not.exist;
       expect(vInfo.error.code, "prop:err.code - invalid").to.equal(kvs.ERROR_CODE.NOT_FOUND);
+      expect(vInfo.value, "prop:value - invalid").to.equal(undefined);
+      expect(vInfo.type, "prop:type - invalid").to.equal(undefined);
    });
 
 
